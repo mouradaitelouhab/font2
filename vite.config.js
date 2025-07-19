eef8ef2167ff0138b11c82a19ce9553e8df471c0
mouradaitelouhab <mouradprotech@gmail.com>
2025-07-18 21:16:58 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,6 +14,16 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5174,
-    allowedHosts: ["localhost", "https://back2-2z57.onrender.com"],
+    allowedHosts: [
+      
+      "https://back2-2z57.onrender.com"
+    ],
+    proxy: {
+      '/api': {
+        target: 'https://back2-2z57.onrender.com/api',  // your backend URL for local dev
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
